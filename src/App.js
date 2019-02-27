e3757ade77d613bcb9ff34e3f96d0da4fd075eba
belselios@gmail.com <belselios@gmail.com>
2019-02-28 07:04:15 +0900

    chapter 4 create customer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customer = {
+  name: "홍길순",
+  birthDay: "919291",
+  gender: "남자",
+  job: "대학생"
+};
 
 class App extends Component {
   render() {
-    return <div className="App">고객관리 시스템</div>;
+    return (
+      <Customer
+        name={customer.name}
+        birthDay={customer.birthDay}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
   }
 }
 
