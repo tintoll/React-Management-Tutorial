7535a4d66c5768dd95d61a63c0545d8569cb5d07
belselios@gmail.com <belselios@gmail.com>
2019-02-28 07:16:04 +0900

    chaper 5 Component splting

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React, { Component } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "홍길순",
-  birthDay: "919291",
-  gender: "남자",
-  job: "대학생"
-};
-
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "나동빈",
+    birthday: "960508",
+    gender: "남자",
+    job: "프로그래머"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "이순신",
+    birthday: "961127",
+    gender: "남자",
+    job: "디자이너"
+  }
+];
+// map함수는 특정한 리스트(List)가 있을 때 해당 리스트의 각 원소에 특정한 문법을 적용한 결과 리스트를 반환합니다.
+// 맵(Map)을 이용해 다수의 정보를 출력할 때는 key라는 이름의 Props를 사용해야 한다는 점입니다.
+// 이를 사용하지 않으면 자바스크립트 콘솔(Console)에 관련 오류가 출력됩니다.
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthDay={customer.birthDay}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {customers.map(customer => {
+          return (
+            <Customer
+              key={customer.id}
+              id={customer.id}
+              image={customer.image}
+              name={customer.name}
+              birthday={customer.birthday}
+              gender={customer.gender}
+              job={customer.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
